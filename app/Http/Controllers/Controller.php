525fa0403672ce4dd59792355eab7487ed100ab9
Giovanni <giovanniw30@hotmail.com>
2023-09-03 20:17:25 -0300

    add pre-commit configuration

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,23 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    //    UNCOMENT LINES BELOW TO TEST PRE-COMMIT BEHAVIOUR
+    //    public function testPintDontWork(){
+    //    return [
+    //    "item1"=>1,
+    //    "item2"=>2,
+    //    "item3"=>3,
+    //    "item4"=>4,
+    //    ];
+    //    }
+    public function testPintWorks()
+    {
+        return [
+            'item1' => 1,
+            'item2' => 2,
+            'item3' => 3,
+            'item4' => 4,
+        ];
+    }
 }
